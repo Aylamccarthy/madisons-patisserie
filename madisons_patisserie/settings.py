02bc804f4bad65a2c4050052a4a8639cd304be6b
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-09-21 18:52:54 +0000

    Create home app and templates

--- madisons_patisserie/settings.py
+++ madisons_patisserie/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'home',
 
     # Other
     'storages',
@@ -68,7 +69,10 @@ ROOT_URLCONF = 'madisons_patisserie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
