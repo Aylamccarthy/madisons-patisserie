41b1405b042747351ccb7ce82772dd54c7cdba0e
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-09-27 19:40:11 +0000

    Add subscription form to context processors

---
+++ madisons_patisserie/context_processors.py
@@ -0,0 +1,12 @@
+"""
+madisons_patisserie - Context processors
+"""
+from newsletter.forms import AddSubscriber
+
+
+def add_subscription_form_to_context(request):
+    """Method to return subscription form as context"""
+
+    return {
+        'add_subscriber_form': AddSubscriber
+    }
