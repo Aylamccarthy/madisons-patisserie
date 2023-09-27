a2e07961af31abd7bd83f99f9c1359d157709e32
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-09-27 19:16:46 +0000

    Create urls.py for newsletter app

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -6,6 +6,7 @@ Forms for Newsletter App.
 from django import forms
 from .models import Subscription
 
+
 class AddSubscriber(forms.ModelForm):
     """Form for adding email to newsletter subscribers"""
     email = forms.EmailField(widget=forms.TextInput(attrs={'id':'newsletter_email',}),label="Enter your email:")
