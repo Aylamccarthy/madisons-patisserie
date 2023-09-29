5d502f4c3eeea023f6bd768a52c652529ebdaa83
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-09-29 10:58:35 +0000

    feat: Create categories and product models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,42 @@
+"""
+Products App - Models
+----------------
+Models for Products App.
+"""
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    """Category model"""
+    class Meta:
+        """Override Meta class"""
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    def get_friendly_name(self):
+        """Method that returns category friendly_name"""
+        return self.friendly_name
+
+
+class Product(models.Model):
+    """Product model"""
+    category = models.ForeignKey('Category', null=True, blank=True,
+                                 on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    code = models.CharField(max_length=6)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
+                                 blank=True)
+    image = models.ImageField(null=True, blank=True)
+    stock = models.IntegerField(default=100)
+
+    def __str__(self):
+        return str(self.name)
