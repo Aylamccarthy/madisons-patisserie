1292271c8264333f513d0f48974610bd05cf7a77
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-10-04 20:20:40 +0000

    feat: Modify UpdateProductForm

--- products/forms.py
+++ products/forms.py
@@ -21,7 +21,6 @@ class UpdateProductForm(forms.ModelForm):
     price = forms.DecimalField(max_digits=6, decimal_places=2)
     image = forms.ImageField(label='Image', required=False,
                              widget=CustomClearableFileInput)
-
     stock = forms.IntegerField(min_value=0)
 
     def __init__(self, *args, **kwargs):
