da64a88dfd28880a18b11b0fc6bfe6ae88b6bc61
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-10-06 19:36:21 +0000

    fix: Refactor code to fix ImportError

--- madisons_patisserie/context_processors.py
+++ madisons_patisserie/context_processors.py
@@ -3,7 +3,7 @@ madisons_patisserie - Context processors
 """
 from django.core.serializers import serialize
 from newsletter.forms import AddSubscriber
-from products.forms import AddUpdateProductForm
+from products.forms import UpdateProductForm
 from products.models import Product
 
 
@@ -12,14 +12,12 @@ def add_subscription_form_to_context(request):
 
     return {"add_subscriber_form": AddSubscriber}
 
+
 def add_create_product_form_to_context(request):
     """Method to return a form for adding a new product as context"""
-    return {
-        'add_product_form': AddUpdateProductForm(prefix='ADD')
-    }
+    return {"add_product_form": UpdateProductForm(prefix="ADD")}
+
 
 def add_products_list_to_context(request):
     """Method to return a list of products as context"""
-    return {
-        'products_json': serialize('json', Product.objects.all())
-    }
+    return {"products_json": serialize("json", Product.objects.all())}
