8ebfa010c2bee08e0189e96af20f7f82ed8dbbae
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-10-16 19:58:50 +0000

    Add checkout urls to main project urls

--- madisons_patisserie/urls.py
+++ madisons_patisserie/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path("subscribe/", include("newsletter.urls")),
     path("/", include("product_reviews.urls")),
     path("bag/", include("bag.urls")),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
