c59a693c3ea557d02120823c144700a75943e01a
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-10-21 21:03:34 +0000

    Change select and input style

--- checkout/urls.py
+++ checkout/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
         views.CheckoutSuccess.as_view(),
         name="checkout_success",
     ),
+    path('cache_checkout_data/', views.CacheCheckoutData.as_view(),
+         name='cache_checkout_data'),
+    path('wh/', webhook, name='webhook'),
 ]
