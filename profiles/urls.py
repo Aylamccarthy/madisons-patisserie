e40345dce1efeba685bf2337d8d90b83223e1dee
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-10-23 20:19:55 +0000

    Update profiles app urls

--- profiles/urls.py
+++ profiles/urls.py
@@ -9,4 +9,9 @@ from . import views
 urlpatterns = [
     path('', views.Profile.as_view(),
          name='profile'),
+    path('delivery_upate/<user_pk>/',
+         views.ProfileDeliveryUpdate.as_view(),
+         name='profile_delivery_update'),
+    path('order_details/<order_number>', views.OrderDetails.as_view(),
+         name='order_details'),
 ]
