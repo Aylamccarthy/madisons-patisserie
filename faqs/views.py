5c0d27a04872523935c70cb580129fd280fa86ed
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-10-29 19:40:41 +0000

    Create views for faqs app

--- faqs/views.py
+++ faqs/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from .models import FAQ
+
+
+def faq_list(request):
+    faqs = FAQ.objects.all()
+    return render(request, 'faqs/faq_list.html', {'faqs': faqs})
