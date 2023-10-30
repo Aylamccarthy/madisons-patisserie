ed9ba90811af02192061b97e8567835ba7e24eac
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-10-30 22:35:16 +0000

    Update phone number fields

--- profiles/models.py
+++ profiles/models.py
@@ -10,6 +10,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class UserProfile(models.Model):
@@ -19,7 +20,7 @@ class UserProfile(models.Model):
     """
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_phone_number = PhoneNumberField(max_length=20, null=True,)
     default_country = CountryField(blank_label="Country", null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
